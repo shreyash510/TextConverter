2bc2c7f3ec67ca4bf64d8ed73da4292a6c9e2d1d
Shreyash Kolhe <shreyashkolhe2001@gmail.com>
2021-12-12 22:20:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CardApp from './CardApp';
+import './App.css';
+import '../src/Style/cardView.css';
+
+function App() {
+  return (
+   <>
+   <CardApp/>
+   </>
+  );
+}
+
+export default App;
